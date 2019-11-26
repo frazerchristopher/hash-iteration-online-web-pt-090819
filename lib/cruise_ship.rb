3e27cdb7cc7044d99321c567e2b47c58ec483371
Christopher L Frazer <frazerchristopher@outlook.com>
2019-11-26 06:43:02 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,16 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+require_relative "../lib/cruise_ship.rb"
 
-def select_winner(passengers)
-  # add the code snippet here!
-end
+
+describe "#select_winner" do
+  it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do 
+
+    passengers = {
+      suite_a: "Amanda Presley", 
+      suite_b: "Seymour Hoffman", 
+      suite_c: "Alfred Tennyson", 
+      suite_d: "Charlie Chaplin", 
+      suite_e: "Crumpet the Elf"
+      }
+
+    expect(select_winner(passengers)).to eq("Amanda Presley")
+  end
